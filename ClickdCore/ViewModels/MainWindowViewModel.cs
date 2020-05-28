7b532e8437fda09b8f0d5504b8189b8a1afc9806
Brandon <brandoneugenewalker@outlook.com>
2020-05-28 14:58:42 -0400

    Initial commit.

---
+++ ClickdCore/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClickdCore.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel() 
+        {
+
+        }
+
+        public void AboutCommand()
+        {
+            //TODO
+        }
+    }
+}
